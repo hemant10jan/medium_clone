3d700d9d5ca36526f59f3bd735d7d4dfee13226c
Hemant Singh <hemant10jan@gmail.com>
2025-01-29 14:36:35 +0530

    Sample 1

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { useRecoilValue } from "recoil";
-import { Blogs } from "./Blogs";
-import { Signin } from "./Signin";
+// import { Blogs } from "./Blogs";
+// import { Signin } from "./Signin";
 import { idState } from "../state/atom";
 
 export const Home=()=>{
     const currentUserLoggedinId=useRecoilValue(idState)
+    console.log(currentUserLoggedinId)
 
     return <div>Hi</div>
 
